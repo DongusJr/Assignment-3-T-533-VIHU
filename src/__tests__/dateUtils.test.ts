25e88ec5b4f38f34c62eb724581f17c3a6cf540a
Dongus <bjarkim19@ru.is>
2022-02-14 20:24:25 +0000

    made code prettier

--- src/__tests__/dateUtils.test.ts
+++ src/__tests__/dateUtils.test.ts
@@ -1,161 +1,164 @@
 import { isDate } from "moment";
 import { DATE_UNIT_TYPES } from "../constants";
-import { getCurrentYear, add, isWithinRange, isDateBefore, isSameDay } from "../dateUtils";
-
+import {
+  getCurrentYear,
+  add,
+  isWithinRange,
+  isDateBefore,
+  isSameDay,
+} from "../dateUtils";
 
 // Date Utils -> Coverage 100%
 
 describe("Date Utils", () => {
-
   // getCurrentYear()
 
-  it('should return the current year as a number', () => {
+  it("should return the current year as a number", () => {
     const result = getCurrentYear();
     expect(result).toBe(2022);
   });
 
   // add(randomDate, incrementNumber, timeType)
 
-  it('should add a certain date with 30 days and return date after 30 days', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should add a certain date with 30 days and return date after 30 days", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let incrementNumber = 30;
     let timeType = DATE_UNIT_TYPES.DAYS;
-    let expectedDate = new Date(1999,1,7,1,2,3,4);
+    let expectedDate = new Date(1999, 1, 7, 1, 2, 3, 4);
     const result = add(randomDate, incrementNumber, timeType);
     expect(result).toEqual(expectedDate);
   });
 
-  it('should add a certain date with 0 days and return the same date', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should add a certain date with 0 days and return the same date", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let incrementNumber = 0;
     let timeType = DATE_UNIT_TYPES.DAYS;
-    let expectedDate = new Date(1999,0,8,1,2,3,4);
+    let expectedDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     const result = add(randomDate, incrementNumber, timeType);
     expect(result).toEqual(expectedDate);
   });
 
-  it('should add a certain date with 59 seconds and return date after 59 seconds', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should add a certain date with 59 seconds and return date after 59 seconds", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let incrementNumber = 59;
     let timeType = DATE_UNIT_TYPES.SECONDS;
-    let expectedDate = new Date(1999,0,8,1,3,2,4);
+    let expectedDate = new Date(1999, 0, 8, 1, 3, 2, 4);
     const result = add(randomDate, incrementNumber, timeType);
     expect(result).toEqual(expectedDate);
   });
 
-  it('should add a certain date with 59 minutes and return date after 59 minutes', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should add a certain date with 59 minutes and return date after 59 minutes", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let incrementNumber = 59;
     let timeType = DATE_UNIT_TYPES.MINUTES;
-    let expectedDate = new Date(1999,0,8,2,1,3,4);
+    let expectedDate = new Date(1999, 0, 8, 2, 1, 3, 4);
     const result = add(randomDate, incrementNumber, timeType);
     expect(result).toEqual(expectedDate);
   });
 
-  it('should add a certain date with 5 years and return date after 5 years', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should add a certain date with 5 years and return date after 5 years", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let incrementNumber = 5;
     let timeType = DATE_UNIT_TYPES.YEARS;
-    let expectedDate = new Date(2004,0,8,1,2,3,4);
+    let expectedDate = new Date(2004, 0, 8, 1, 2, 3, 4);
     const result = add(randomDate, incrementNumber, timeType);
     expect(result).toEqual(expectedDate);
   });
 
   // isWithinRange(betweenDate, beforeDate, afterDate)
 
-  it('should check a date that is between two dates and return true value', () => {
-    let betweenDate = new Date(1999,0,8,1,2,3,4);
-    let beforeDate = new Date(1997,0,28,1,2,3,4);
-    let afterDate = new Date(2001,4,30,1,2,3,4);
+  it("should check a date that is between two dates and return true value", () => {
+    let betweenDate = new Date(1999, 0, 8, 1, 2, 3, 4);
+    let beforeDate = new Date(1997, 0, 28, 1, 2, 3, 4);
+    let afterDate = new Date(2001, 4, 30, 1, 2, 3, 4);
     let result = isWithinRange(betweenDate, beforeDate, afterDate);
     expect(result).toBeTruthy();
   });
 
-  it('should check a date that is not between two dates and return false value', () => {
-    let betweenDate = new Date(1992,8,14,1,2,3,4);
-    let beforeDate = new Date(1997,0,28,1,2,3,4);
-    let afterDate = new Date(2001,4,30,1,2,3,4);
+  it("should check a date that is not between two dates and return false value", () => {
+    let betweenDate = new Date(1992, 8, 14, 1, 2, 3, 4);
+    let beforeDate = new Date(1997, 0, 28, 1, 2, 3, 4);
+    let afterDate = new Date(2001, 4, 30, 1, 2, 3, 4);
     let result = isWithinRange(betweenDate, beforeDate, afterDate);
     expect(result).toBeFalsy();
   });
-  
-  it('should check a date between two undefined dates returns false', () => {
-    let betweenDate = new Date(1992,8,14,1,2,3,4);
+
+  it("should check a date between two undefined dates returns false", () => {
+    let betweenDate = new Date(1992, 8, 14, 1, 2, 3, 4);
     let beforeDate = undefined;
     let afterDate = undefined;
     let result = isWithinRange(betweenDate, beforeDate, afterDate);
     expect(result).toBeFalsy();
   });
 
-  it('should check a date between same date as itself returns false', () => {
-    let betweenDate = new Date(1999,0,8,1,2,3,4);
+  it("should check a date between same date as itself returns false", () => {
+    let betweenDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let result = isWithinRange(betweenDate, betweenDate, betweenDate);
     expect(result).toBeFalsy();
   });
 
   // isDateBefore(randomDate, compareDate)
 
-  it('should check a date with a date that came before it and return true', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
-    let beforeDate = new Date(1997,0,28,1,2,3,4);
+  it("should check a date with a date that came before it and return true", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
+    let beforeDate = new Date(1997, 0, 28, 1, 2, 3, 4);
     let result = isDateBefore(beforeDate, randomDate);
     expect(result).toBeTruthy();
   });
 
-  it('should check a date with a date that came after it and return false', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
-    let beforeDate = new Date(2001,4,30,1,2,3,4);
+  it("should check a date with a date that came after it and return false", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
+    let beforeDate = new Date(2001, 4, 30, 1, 2, 3, 4);
     let result = isDateBefore(beforeDate, randomDate);
     expect(result).toBeFalsy();
   });
 
-  it('should check a date with an undefined and return false', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should check a date with an undefined and return false", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let beforeDate = undefined;
     let result = isDateBefore(beforeDate, randomDate);
     expect(result).toBeFalsy();
   });
 
-  it('should check a date with itself and return false', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should check a date with itself and return false", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let result = isDateBefore(randomDate, randomDate);
     expect(result).toBeFalsy();
   });
 
   // isSameDay(Date, compareDate)
 
-  it('should check a date with itself and return true', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should check a date with itself and return true", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let result = isSameDay(randomDate, randomDate);
     expect(result).toBeTruthy();
   });
 
-  it('should check a date with another instance of itself and return true', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
-    let randomDate2 = new Date(1999,0,8,1,2,3,4);
+  it("should check a date with another instance of itself and return true", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
+    let randomDate2 = new Date(1999, 0, 8, 1, 2, 3, 4);
     let result = isSameDay(randomDate, randomDate2);
     expect(result).toBeTruthy();
   });
 
-  it('should check a date with a date that came before it and return false', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
-    let beforeDate = new Date(1997,0,28,1,2,3,4);
+  it("should check a date with a date that came before it and return false", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
+    let beforeDate = new Date(1997, 0, 28, 1, 2, 3, 4);
     let result = isSameDay(randomDate, beforeDate);
     expect(result).toBeFalsy();
   });
 
-  it('should check a date with a date that came before it and return false', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
-    let afterDate = new Date(2001,4,30,1,2,3,4);
+  it("should check a date with a date that came before it and return false", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
+    let afterDate = new Date(2001, 4, 30, 1, 2, 3, 4);
     let result = isSameDay(randomDate, afterDate);
     expect(result).toBeFalsy();
   });
 
-  it('should check a date with an undefined and return false', () => {
-    let randomDate = new Date(1999,0,8,1,2,3,4);
+  it("should check a date with an undefined and return false", () => {
+    let randomDate = new Date(1999, 0, 8, 1, 2, 3, 4);
     let randomDate2 = undefined;
     let result = isSameDay(randomDate, randomDate2);
     expect(result).toBeFalsy();
   });
-  
-})
+});
--- src/dateUtils.ts
+++ src/dateUtils.ts
@@ -1,34 +1,34 @@
-
 import * as datefns from "date-fns";
 import { DATE_UNIT_TYPES } from "./constants";
 
-
-
-export function getDurationFromType(number: number, type: DATE_UNIT_TYPES): Duration {
+export function getDurationFromType(
+  number: number,
+  type: DATE_UNIT_TYPES
+): Duration {
   // I find this solution messy, I tried to do {DATE_UNIT_TYPES[type] : number} so it could only
   // be in one line, however, I never got it to work, even though the documentation and
   // stackoverflow tells me it works.
-  switch(type){
+  switch (type) {
     case DATE_UNIT_TYPES.SECONDS: {
-      return {seconds: number};
+      return { seconds: number };
     }
     case DATE_UNIT_TYPES.MINUTES: {
-      return {minutes: number};
+      return { minutes: number };
     }
     case DATE_UNIT_TYPES.DAYS: {
-      return {days: number};
+      return { days: number };
     }
     case DATE_UNIT_TYPES.DAYS: {
-      return {days: number};
+      return { days: number };
     }
     case DATE_UNIT_TYPES.WEEKS: {
-      return {weeks: number};
+      return { weeks: number };
     }
     case DATE_UNIT_TYPES.MONTHS: {
-      return {months: number};
+      return { months: number };
     }
     case DATE_UNIT_TYPES.YEARS: {
-      return {years: number};
+      return { years: number };
     }
     default: {
       return undefined;
@@ -40,17 +40,26 @@ export function getCurrentYear(): number {
   return datefns.getYear(new Date());
 }
 
-export function add(date: Date, number: number, type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS): Date {
+export function add(
+  date: Date,
+  number: number,
+  type: DATE_UNIT_TYPES = DATE_UNIT_TYPES.DAYS
+): Date {
   return datefns.add(date, getDurationFromType(number, type));
 }
 
 export function isWithinRange(date: Date, from: Date, to: Date): boolean {
   // When I wrote the tests for the moment library, he returned false for undefined dates or
   // if the date was the same for both from and to, which is debatable if it should return true or false
-  if ((isSameDay(date,from) && isSameDay(date, to)) || (date === undefined || from === undefined || to === undefined)) {
-    return false
+  if (
+    (isSameDay(date, from) && isSameDay(date, to)) ||
+    date === undefined ||
+    from === undefined ||
+    to === undefined
+  ) {
+    return false;
   } else {
-    return datefns.isWithinInterval(date, {start: from, end: to});
+    return datefns.isWithinInterval(date, { start: from, end: to });
   }
 }
 
